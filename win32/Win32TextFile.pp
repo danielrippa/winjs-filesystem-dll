068d41df987535e9b353f801afbc5e0f39d1a9f0
danielrippa <621091+danielrippa@users.noreply.github.com>
2023-03-04 03:02:21 -0300

    .

--- win32/Win32TextFile.pp
+++ win32/Win32TextFile.pp
@@ -1,1 +1,61 @@
+unit Win32TextFile;
 
+{$mode delphi}
+
+interface
+
+  function ReadTextFileContent(FilePath: WideString; out Content: WideString): Boolean;
+  function WriteTextFileContent(FilePath, Content: WideString): Boolean;
+
+implementation
+
+  uses
+    Classes;
+
+  function ReadTextFileContent;
+  var
+    FileStream: TFileStream;
+    S: UTF8String;
+  begin
+    Result := False;
+
+    FileStream := TFileStream.Create(FilePath, fmOpenRead);
+    try
+
+      if FileStream.Size = 0 then Exit;
+
+      with FileStream do begin
+        SetLength(S, Size);
+        Read(S[1], Size);
+
+        Content := UTF8String(S);
+
+        Result := True;
+      end;
+
+    finally
+      FileStream.Free;
+    end;
+
+  end;
+
+
+  function WriteTextFileContent;
+  var
+    FileContent: String;
+  begin
+    Result := False;
+    try
+      FileContent := UTF8Encode(Content);
+      with TFileStream.Create(FilePath, fmCreate) do begin
+        Write(FileContent[1], Length(FileContent));
+        Free;
+      end;
+
+      Result := True;
+    except
+    end;
+  end;
+
+
+end.
